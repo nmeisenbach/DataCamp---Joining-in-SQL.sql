d59bb01bd767cfb03126e516fd48919318cf6597
nmeisenbach <110125762+nmeisenbach@users.noreply.github.com>
2022-08-22 12:11:59 -0400

    Update Joining in sql.sql

--- Joining in sql.sql
+++ Joining in sql.sql
@@ -63,5 +63,45 @@ SELECT name, continent, code, surface_area,
         ELSE 'small' END
         AS geosize_group
 FROM countries;
+
+SELECT p.country_code, p.size,
+    CASE WHEN size > 50000000 
+        THEN 'large'
+    WHEN size > 1000000 
+        THEN 'medium'
+    ELSE 'small'END
+    AS popsize_group
+FROM populations AS p
+WHERE year = 2015;
+
+SELECT country_code, size,
+    CASE WHEN size > 50000000 THEN 'large'
+        WHEN size > 1000000 THEN 'medium'
+        ELSE 'small' END
+        AS popsize_group
+INTO pop_plus
+FROM populations
+WHERE year = 2015;
+
+
+SELECT * FROM
+pop_plus;
+
+SELECT country_code, size,
+  CASE WHEN size > 50000000
+            THEN 'large'
+       WHEN size > 1000000
+            THEN 'medium'
+       ELSE 'small' END
+       AS popsize_group
+INTO pop_plus       
+FROM populations
+WHERE year = 2015;
+
+SELECT name, continent, geosize_group, popsize_group
+FROM countries_plus AS c
+INNER JOIN pop_plus AS p
+  ON c.code = p.country_code
+ORDER BY geosize_group
  
  
