963c4e3f2284390aa261f3b032f14146fbc527a6
nmeisenbach <110125762+nmeisenbach@users.noreply.github.com>
2022-08-21 20:09:14 -0400

    Update Joining in sql.sql

--- Joining in sql.sql
+++ Joining in sql.sql
@@ -1,6 +1,8 @@
 # DataCamp---Joining-in-SQL.sql
 SQL Joining Data in SQL
 
+Inner Joins
+
 SELECT * 
 FROM cities;
 
@@ -13,3 +15,15 @@ SELECT cities.name AS city, countries.name AS country, region
 FROM cities 
 INNER JOIN countries 
 ON cities.country_code = countries.code;
+
+SELECT c.code, c.name, c.region, p.year, p.fertility_rate
+FROM countries AS c
+  INNER JOIN populations AS p
+    ON c.code = p.country_code
+    
+SELECT c.code, c.name, c.region, e.year, p.fertility_rate, e.unemployment_rate
+  FROM countries AS c
+  INNER JOIN populations AS p
+    ON c.code = p.country_code
+  INNER JOIN economies AS e
+    ON c.code = e.code 
