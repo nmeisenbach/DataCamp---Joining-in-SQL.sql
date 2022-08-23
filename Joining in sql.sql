f53b5334ad0e84d492ca5c63bdb86a1e1b002fd4
nmeisenbach <110125762+nmeisenbach@users.noreply.github.com>
2022-08-23 11:50:23 -0400

    Update Joining in sql.sql

--- Joining in sql.sql
+++ Joining in sql.sql
@@ -158,3 +158,19 @@ FROM countries
 WHERE region = 'North America' OR region IS NULL
 -- Order by region
 ORDER BY region;
+
+--Outer challenge
+-- Select fields
+SELECT c.name AS country, region, life_expectancy AS life_exp
+-- From countries (alias as c)
+FROM countries AS c
+  -- Join to populations (alias as p)
+  LEFT JOIN populations AS p
+    -- Match on country code
+    ON c.code = p.country_code
+-- Focus on 2010
+WHERE year = 2010
+-- Order by life_exp
+ORDER BY life_exp
+-- Limit to 5 records
+LIMIT 5
