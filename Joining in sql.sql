fc3d9f47107afc21b545fd2812d12b55c8dc485e
nmeisenbach <110125762+nmeisenbach@users.noreply.github.com>
2022-08-24 08:23:04 -0400

    Update Joining in sql.sql

--- Joining in sql.sql
+++ Joining in sql.sql
@@ -200,3 +200,16 @@ WHERE code IN
    WHERE region = 'Middle East')
 -- Order by name
 ORDER BY name;
+
+-- Select fields
+SELECT local_name, lang_num
+  -- From countries
+  FROM countries,
+  	-- Subquery (alias as subquery)
+  	(SELECT code, COUNT(*) AS lang_num
+  	 FROM languages
+  	 GROUP BY code) AS subquery
+  -- Where codes match
+  WHERE countries.code = subquery.code
+-- Order by descending number of languages
+ORDER BY lang_num DESC
